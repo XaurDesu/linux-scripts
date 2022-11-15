5e17c90c72f18739e6776016c7486f2533bd5155
Jaime Torres <andrestbermejoj@gmail.com>
2022-11-15 14:55:01 -0500

    add misc software to Fedora script

--- Red Hat/Fedora/build_my_system.sh
+++ Red Hat/Fedora/build_my_system.sh
@@ -6,6 +6,8 @@ echo "installing KDE Plasma..."
 sudo bash ./individual_scripts/install_kde.sh
 echo "installing VSCode..."
 sudo bash ./individual_scripts/install_vscode.sh
+echo "installing misc software..."
+sudo bash ./individual_scripts/install_misc.sh
 echo "Updating System..." 
 sudo dnf update -y
 sudo dnf upgrade -y
---
+++ Red Hat/Fedora/individual_scripts/install_misc.sh
@@ -0,0 +1,11 @@
+echo "configuring Flatpak"
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+echo "installing Discord"
+flatpak install flathub com.spotify.Client -y
+echo "installing Spotify"
+flatpak install flathub com.discordapp.Discord -y
+echo "installing neovim"
+sudo dnf install neovim -y
+echo "installing Steam"
+flatpak install flathub com.valvesoftware.Steam -y
