4737b561d8af3bd8a589ca1d34c60b3ca00102e4
Jaime Andres Torres B <69700780+XaurDesu@users.noreply.github.com>
2022-11-26 20:51:34 -0500

    add functions

---
+++ Debian⁄Ubuntu/build_my_system.sh
@@ -0,0 +1,10 @@
+echo "installing langs..."
+sudo bash ./individual_scripts/install_langs.sh
+echo "installing misc software"
+sudo bash ./individual_scripts/install_misc.sh
+echo "installing VSCode"
+sudo bash ./individual_scripts/install_vscode.sh
+echo "installing Docker..."
+sudo bash ./individual_scripts/installdocker.sh
+echo "intalling ROS"
+sudo bash ./individual_scripts/installros.sh
--- Debian⁄Ubuntu/individual_scripts/installdocker.sh
+++ Debian⁄Ubuntu/individual_scripts/installdocker.sh
@@ -1,4 +1,4 @@
-sudo apt update
+sudo apt update -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu jammy stable"
 sudo apt install apt-transport-https curl gnupg-agent ca-certificates software-properties-common -y
