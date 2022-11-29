52e32d406c2e42fdf0605229f2087e5ae8cdfafd
Jaime Andres Torres B <69700780+XaurDesu@users.noreply.github.com>
2022-11-28 20:57:57 -0500

    Update install_langs.sh

--- Debian⁄Ubuntu/individual_scripts/install_langs.sh
+++ Debian⁄Ubuntu/individual_scripts/install_langs.sh
@@ -1,3 +1,2 @@
-sudo apt install golang golang-go gccgo-go gcc git python3 python3-pip default-jre default-jdk nodejs
+sudo apt install golang golang gcc git python3 python3-pip default-jre default-jdk nodejs
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-echo "1"
