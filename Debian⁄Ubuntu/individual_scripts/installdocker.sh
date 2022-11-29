5f65d196d0e9f343a96b4e7be2f491aab90bbf87
Jaime Andres Torres B <69700780+XaurDesu@users.noreply.github.com>
2022-11-28 20:06:55 -0500

    Update installdocker.sh

--- Debian⁄Ubuntu/individual_scripts/installdocker.sh
+++ Debian⁄Ubuntu/individual_scripts/installdocker.sh
@@ -1,7 +1,7 @@
 sudo apt update -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu jammy stable"
-sudo apt install apt-transport-https curl gnupg-agent ca-certificates software-properties-common -y
-sudo apt install docker-ce docker-ce-cli containerd.io -y
+sudo apt install apt-transport-https curl gnupg-agent ca-certificates software-properties-common
+sudo apt install docker-ce docker-ce-cli containerd.io
 sudo systemctl enable docker
 sudo systemctl start docker
