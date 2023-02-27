bf08db9dd280f29388b9b043cc188abc3b63fb97
Jaime Andres Torres Bermejo <69700780+XaurDesu@users.noreply.github.com>
2023-02-27 00:29:08 -0500

    add docker

--- Arch/build_my_system.sh
+++ Arch/build_my_system.sh
@@ -1,4 +1,4 @@
-sudo pacman -Syu git gcc python python-pip rust go neovim neofetch jre-openjdk 
+sudo pacman -Syu git gcc python python-pip rust go neovim neofetch jre-openjdk docker
 sudo pacman -S --needed base-devel
 git clone https://aur.archlinux.org/paru.git
 cd paru
