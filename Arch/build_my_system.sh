4c3eb0096d3580e55f1201c0e2e13ffd9269dfcc
Jaime Andres Torres Bermejo <69700780+XaurDesu@users.noreply.github.com>
2023-02-27 00:44:07 -0500

    fix a bug

--- Arch/build_my_system.sh
+++ Arch/build_my_system.sh
@@ -1,7 +1,8 @@
 sudo pacman -Syu git gcc python python-pip rust go neovim neofetch jre-openjdk docker texlive-most
 sudo pacman -S --needed base-devel
 git clone https://aur.archlinux.org/paru.git
+cd /..
 cd paru
-makepkg -si
+
 cd Documentos/
 mkdir repos
