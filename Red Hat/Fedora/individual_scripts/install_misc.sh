44418f77c6f4f8248dc1a04a36d805026c211379
Jaime Andres Torres Bermejo <69700780+XaurDesu@users.noreply.github.com>
2023-02-28 00:26:58 -0500

    Update install_misc.sh

--- Red Hat/Fedora/individual_scripts/install_misc.sh
+++ Red Hat/Fedora/individual_scripts/install_misc.sh
@@ -11,10 +11,5 @@ echo "installing Steam"
 flatpak install flathub com.valvesoftware.Steam -y
 echo "installing Telegram"
 flatpak install flathub org.telegram.desktop -y
-echo "Installing GNOME Tweaks"
-sudo dnf install gnome-tweak-tool -y
-flatpak install flathub org.gnome.Extensions -y
-echo "Installing Theming..."
-sudo dnf install flat-remix-theme -y
 echo "installing LaTeX..."
 sudo dnf install texlive-scheme-full -y
