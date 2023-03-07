7a6ea3fb7de62f38a0097210d2b6879c26621670
Jaime Andres Torres Bermejo <69700780+XaurDesu@users.noreply.github.com>
2023-03-07 18:39:22 -0500

    Update install_misc.sh

--- Red Hat/Fedora/individual_scripts/install_misc.sh
+++ Red Hat/Fedora/individual_scripts/install_misc.sh
@@ -1,6 +1,13 @@
 echo "configuring Flatpak"
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
+echo "Installing GNOME Tweaks"
+sudo dnf install gnome-tweak-tool -y
+flatpak install flathub org.gnome.Extensions -y
+
+echo "Installing Theming..."
+
+sudo dnf install flat-remix-theme -y
 echo "installing Discord"
 flatpak install flathub com.spotify.Client -y
 echo "installing Spotify"
@@ -9,7 +16,5 @@ echo "installing neovim"
 sudo dnf install neovim -y
 echo "installing Steam"
 flatpak install flathub com.valvesoftware.Steam -y
-echo "installing Telegram"
-flatpak install flathub org.telegram.desktop -y
 echo "installing LaTeX..."
 sudo dnf install texlive-scheme-full -y
