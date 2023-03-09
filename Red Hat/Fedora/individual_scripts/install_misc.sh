6e9cf4f25b93f5a2903aa545391164bdc9ad8c52
Jaime Andres Torres Bermejo <69700780+XaurDesu@users.noreply.github.com>
2023-03-09 15:04:10 -0500

    Update install_misc.sh

--- Red Hat/Fedora/individual_scripts/install_misc.sh
+++ Red Hat/Fedora/individual_scripts/install_misc.sh
@@ -1,17 +1,20 @@
 echo "configuring Flatpak"
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
+echo "configuring Snaps..."
+sudo dnf install snapd
+sudo ln -s /var/lib/snapd/snap /snap
+
+echo "installing snaps..."
+sudo snap install spotify discord zulip krita
+
 echo "Installing GNOME Tweaks"
 sudo dnf install gnome-tweak-tool -y
 flatpak install flathub org.gnome.Extensions -y
 
 echo "Installing Theming..."
-
 sudo dnf install flat-remix-theme -y
-echo "installing Discord"
-flatpak install flathub com.spotify.Client -y
-echo "installing Spotify"
-flatpak install flathub com.discordapp.Discord -y
+
 echo "installing neovim"
 sudo dnf install neovim -y
 echo "installing Steam"
